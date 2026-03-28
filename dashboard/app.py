5d1f43d0a04cc77573a1c80ca8b23a70dfee8ba1
Samuel-S06 <109887822+Samuel-S06@users.noreply.github.com>
2026-03-28 16:30:41 -0400

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -50,4 +50,5 @@ def set_medication():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    # app.run(debug=True, host='0.0.0.0', port=5001)
+    pass
